09316df3da37a59d3801d9af5feade18c9508bd7
CSUS-Scrumbags <47368440+CSUS-Scrumbags@users.noreply.github.com>
2019-03-19 18:30:05 -0700

    Update settings.py

--- safelife/safelife_project/safelife_project/settings.py
+++ safelife/safelife_project/safelife_project/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'attendance',
+    'student',
+    'classes'
 ]
 
 MIDDLEWARE = [
@@ -72,13 +75,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'safelife_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+#YOUR USER AND PASSWORD IS YOUR MYSQLWORKBENCH STUFF, ALSO CHECK YOUR PORT.
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangoproject',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
