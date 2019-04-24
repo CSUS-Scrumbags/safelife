1f6b6a7cac1dd33c5a23a6e8a05dacc9a3130be1
slmdg <muayad925@yahoo.com>
2019-04-24 12:57:58 -0700

    Reporting updates

--- safelife/safelife_project/reporting/urls.py
+++ safelife/safelife_project/reporting/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    #url('', views.index, name='teacher-reporting'),
-    url('', views.report, name='teacher-reporting'),
+    path('reports/', views.report, name ='reports'),
+    path('', views.index, name='index'),
+    path('', views.report, name='teacher-reporting'),
     #path('<int:course_id>/', views.index, name='reports'),
-    #path('success/', views.success, name = 'success'),
-    path('report/', views.report, name = 'report'),
+    #path('success/', views.success, name = 'success'),   
 ]
