3f2e0573a21284c7ebc5dc8c4a13528d333ecf8e
CSUS-Scrumbags <47368440+CSUS-Scrumbags@users.noreply.github.com>
2019-04-27 21:11:20 -0700

    Update views.py

--- safelife/safelife_project/attendance/views.py
+++ safelife/safelife_project/attendance/views.py
@@ -19,7 +19,7 @@ def index(request, course_id, date):
                        'FROM courses '
                        'WHERE course_id = %s', [course_id])
         course_name = cursor.fetchone()
-    if attendance_status is not None:
+    if attendance_status is not None or super is True:
         # Get the current date in the following format: <4 Digit Year>-<2 Digit Month>-<2 Digit Day>
         currentdate = datetime.datetime.strptime(date, '%B %d, %Y').strftime('%Y-%m-%d')
         # Get the name of each student that is enrolled in the class
@@ -37,7 +37,6 @@ def index(request, course_id, date):
                                                                [course_id, currentdate])
         class_dates = Class.objects.filter(course=course_id)
         currentdate = datetime.datetime.strptime(currentdate, '%Y-%m-%d').strftime('%B %d, %Y')
-        
         # Create a template using index.html, and pass into it the list of student names and recorded absences
         if super is True:
             template = loader.get_template('AdminIndex.html')
@@ -119,7 +118,7 @@ def update_student_absent(request, course_id, date):
             # Change the students attendance status from Absent to 'Makeup: <date>'
             cursor.execute('UPDATE attendances '
                            'SET status = "Makeup: " %s '
-                           'WHERE students = %s AND date = %s ', [currentdate, student_data.student_id, date])
+                           'WHERE students = %s AND date = %s ', [request.GET['makeupDate'], student_data.student_id, date])
         else:
             # Change the students attendance status from 'Makeup: <date>' to Absent
             cursor.execute('UPDATE attendances '
@@ -127,4 +126,3 @@ def update_student_absent(request, course_id, date):
                            'WHERE students = %s AND date = %s ', [student_data.student_id, date])
     # Render the response to the user
     return render(request, 'index.html', {})
-
