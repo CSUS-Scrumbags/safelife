08584c5f0c4352907a36656dcd1b9567a9e134aa
CSUS-Scrumbags <47368440+CSUS-Scrumbags@users.noreply.github.com>
2020-10-20 20:29:33 -0700

    Update settings.py

--- safelife/safelife_project/safelife_project/settings.py
+++ safelife/safelife_project/safelife_project/settings.py
@@ -140,9 +140,12 @@ USE_TZ = True
 
 import os.path
 
-STATIC_ROOT = ''
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = ( os.path.join('static'), )
-
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
