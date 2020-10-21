70c67ea12304350b06430709292a395bdacfbfca
Jerome <Scrumbags2018@gmail.com>
2020-10-21 10:55:37 -0700

    Url change
    added an extra path to login page

--- safelife/safelife_project/safelife_project/urls.py
+++ safelife/safelife_project/safelife_project/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('/', include('user.urls')),
+    path('', include('users.urls')),
     path('admin/', admin.site.urls),
     path('administrator/', include('administrator.urls')),
     path('administrator/attendance/', include('attendance.urls')),
