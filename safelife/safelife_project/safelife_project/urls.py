a89e8aca4cdf249f675821d6d9a23ac09f1c5851
CSUS-Scrumbags <47368440+CSUS-Scrumbags@users.noreply.github.com>
2020-10-21 10:45:11 -0700

    Update urls.py

--- safelife/safelife_project/safelife_project/urls.py
+++ safelife/safelife_project/safelife_project/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('/', include('user.urls')),
     path('admin/', admin.site.urls),
     path('administrator/', include('administrator.urls')),
     path('administrator/attendance/', include('attendance.urls')),
